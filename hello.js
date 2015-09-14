ab61c226c7bb3c8a19c02a6f63ff6a3a3b770417
Nguyen Vo <nguyen.vo@ifactorinc.com>
2015-09-14 08:22:16 -0700

    Create one more bug for the branch

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-var http = require('http');
+avar http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n') // this will FAIL travis ci lint
